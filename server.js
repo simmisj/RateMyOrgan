031eb65e97123cdcfd6067b378879f51b07c0280
Simmi Jonsson <simmisj@hotmail.com>
2018-05-25 15:27:09 +0200

    Removed dependency on iot hub.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const WebSocket = require('ws');
 const moment = require('moment');
 const path = require('path');
-const iotHubClient = require('./IoTHub/iot-hub.js');
 
 const app = express();
 
